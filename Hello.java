640f6bf5742c89b94e297f4a1004d059cad0f688
2022sp93047 <145192339+Sim-47@users.noreply.github.com>
2023-09-16 17:37:02 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+	public static void main(String args[])
+	{
+		System.out.println("Hello, World");
+		System.out.println("Simran Roll No: 2022sp93047");
+		System.out.println("Bye, World!");
+	}
+}
